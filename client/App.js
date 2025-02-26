5952acb117b8da9a9c335531f21f040c383ea127
user <user@users-MacBook-Air.local>
2025-02-25 18:02:34 -0800

    added useState hook and color change to toggle switch

--- client/App.js
+++ client/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 import ValveSwitch from "./ValveSwitch";
 
 function App() {
+  const [value, setValue] = useState(false);
   return (
     <div className="app">
-      <ValveSwitch />
+      <ValveSwitch
+        isOn={value}
+        onColor="#EF476F"
+        handleToggle={() => setValue(!value)}
+      />
     </div>
   );
 }
--- client/ValveSwitch.js
+++ client/ValveSwitch.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './ValveSwitch.css';
 
-const ValveSwitch = () => {
+const ValveSwitch = ({ isOn, handleToggle }) => {
   return (
     <>
       <input
+        checked={isOn}
+        onChange={handleToggle}
         className="react-switch-checkbox"
         id={`react-switch-new`}
         type="checkbox"
       />
       <label
+        style={{ background: isOn && '#06D6A0' }}
         className="react-switch-label"
         htmlFor={`react-switch-new`}
       >
